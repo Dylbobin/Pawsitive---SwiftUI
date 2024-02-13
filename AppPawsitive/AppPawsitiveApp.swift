30d171822537b03f799326d4d4b388f62aa76b8c
Dylbobin <dylansilva22@yahoo.com>
2024-02-12 20:31:49 -0800

    Initial Commit

---
+++ AppPawsitive/AppPawsitiveApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppPawsitiveApp.swift
+//  AppPawsitive
+//
+//  Created by Dylan Silva on 2/12/24.
+//
+
+import SwiftUI
+
+@main
+struct AppPawsitiveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AppPawsitive/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AppPawsitive
+//
+//  Created by Dylan Silva on 2/12/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
